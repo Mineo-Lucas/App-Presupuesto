76bf32e5f5b81077ea49fb17d154b6b15f33a70d
unknown <lucas_mineo@outlook.com>
2024-09-09 13:54:32 -0300

    metodos

---
+++ js/App.js
@@ -0,0 +1,17 @@
+const ingreso= [
+    new ingreso(sueldo, 650000),
+    new ingreso(reintegro, 150000)
+]
+
+const egreso= [
+    new egreso(sueldo, 650000),
+    new egreso(reintegro, 150000)
+]
+
+let cargarApp = () =>{
+    cargarCabezero()
+}
+
+let cargarCabezero = () =>{
+    
+}
